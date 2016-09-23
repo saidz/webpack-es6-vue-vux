7a38ac97c6f8d8d6c4b347a0337b710eb83077ff
said <zyx.zhuyuxiang@gmail.com>
2016-09-23 18:07:34 +0800

    add storeList

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import Home from './components/HelloFromVux'
+import StoreList from './components/Store/StoreList'
 import VueRouter from 'vue-router'
-
 const FastClick = require('fastclick')
 FastClick.attach(document.body)
-
 Vue.use(VueRouter)
-
 const router = new VueRouter()
-
 router.map({
   '/': {
     component: Home
+  },
+  '/store_list': {
+    component: StoreList
   }
 })
-
 router.start(App, '#app')
-

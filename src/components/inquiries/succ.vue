73584c43e9b3d7a685aeaa40100aa9c3602803ca
said <zyx.zhuyuxiang@gmail.com>
2016-10-08 19:59:47 +0800

    add inquiries succ.vue module

---
+++ src/components/inquiries/succ.vue
@@ -0,0 +1,13 @@
+<template>
+<article class="commit-inquiry">
+  <section class="tip-result">
+    <i class="result-ico icon-tipico-ok"></i>
+    <p class="result-hd">已提交询价</p>
+    <p class="result-info">报价单将在30分钟内推送给你，请留意。</p>
+  </section>
+  <footer class="footer">
+    <em class="icon-logo-zs">
+    </em>中升集团
+  </footer>
+</article>
+</template>
